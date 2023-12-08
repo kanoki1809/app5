7971c70acfae2d116f07a3fa2d3f2b5dacfad76c
kanoki1809 <144712774+kanoki1809@users.noreply.github.com>
2023-12-08 09:56:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import streamlit as st
+from PIL import Image
+import pickle as pkl
+import numpy as np
 
+
+
+st.title('ứng dụng nhận dạng ảnh chữ số viết tay.')
+input = open('lrc_mnist.pkl','rb')
+model = pkl.load(input)
+
+st.header('image')
+image = st.file_uploader('choose an image:',type=(['png','jpg','jpeg']))
+
+
+if image is not None:
+  image =Image.open(image)
+  st.image(image,caption='test image')
+
+  if st.button('Predict'):
+    image=image.resize((227*227*3,1))
+    vector =np.array(image)
+    label= str((model.predict(vector))[0])
+
+    st.header('ket qua')
+    st.text(label)
